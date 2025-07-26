24e17e68400c92fd33a20531db5d02fd38cad712
grcndev <gleyce7mit@gmail.com>
2025-07-25 22:03:26 -0300

    fetch animes configured

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import QueryProvider from "./query-provider";
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,12 +25,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+         <QueryProvider>
         {children}
+        </QueryProvider>
       </body>
     </html>
   );
